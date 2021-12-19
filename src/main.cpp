1fe1a652ec4c7ec18837e06d768d7704d14603c2
Erlend Syljuåsen <esylju@gmail.com>
2021-12-19 23:19:19 +0100

    use scenarios.cpp instead

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,5 @@
-#include "ising.h"
+#include "scenarios.h"
 
 int main() {
-    Ising model{100, 1000.0, 10000.0};
-
-    for (int i = 0; i < 1000; i++) {
-        model.write_lattice_to_file();
-        model.metropolis(10000);
-    }
+    return 0;
 }
